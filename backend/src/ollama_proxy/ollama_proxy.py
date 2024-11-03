b7e9e885902ce8b63ff6fa08fdd54ce3dd5acf2d
Armaggheddon <alebrum97@gmail.com>
2024-11-03 12:32:42 +0100

    Added additional instruct model to run rerank task on document chunks

--- backend/src/ollama_proxy/ollama_proxy.py
+++ backend/src/ollama_proxy/ollama_proxy.py
@@ -1,4 +1,5 @@
 import json
+import asyncio
 from ollama import AsyncClient
 
 from . import utils
@@ -53,7 +54,9 @@ class OllamaProxy:
         self.chat_model_name = utils.get_chat_model_name()
         self.chat_model_max_input_tokens = utils.get_chat_model_max_input_tokens()
 
-        self.messages = [
+        self.instruct_model_name = utils.get_instruct_model_name()
+
+        self.qa_messages = [
             {"role": "system", "content": prompts.QA_SYSTEM_PROMPT}
         ]
     
@@ -69,7 +72,7 @@ class OllamaProxy:
     
     async def chat(self, user_input: str, context: list[str] = None):
         # TODO: format message to include context
-        _query_with_context = self.messages + [{
+        _query_with_context = self.qa_messages + [{
             "role": "user",
             "content": utils.format_query(context, user_input)
         }]
@@ -81,7 +84,6 @@ class OllamaProxy:
         )
 
         async for chunk in response:
-            # yield chunk["message"]["content"]  # this just returns the text
             yield json.dumps(chunk) + '\n'
 
     async def summarize(self, text: list[str]):
@@ -90,8 +92,29 @@ class OllamaProxy:
             {"role": "user", "content": utils.format_summary_prompt(text)}
         ]
         summary = await self.client.chat(
-            model=self.chat_model_name,
+            model=self.instruct_model_name,
             messages=_summary_task,
         )
 
         return summary["message"]["content"]
+    
+    async def rerank(self, user_query, text_chunks: list[str]) -> list[str]:
+        _rerank_system_prompt = [
+            {"role": "system", "content": prompts.DOCUMENT_RERANK_SYSTEM_PROMPT}
+        ]
+        relevant_document_chunks = []
+        for chunk in text_chunks:
+            _rerank_task = _rerank_system_prompt + [
+                {"role": "user", "content": utils.format_rerank_prompt(chunk, user_query)}
+            ]
+
+            rerank_response = await self.client.chat(
+                model=self.instruct_model_name,
+                messages=_rerank_task,
+            )
+
+            is_relevant: bool = "yes" in rerank_response["message"]["content"]
+            if is_relevant:
+                relevant_document_chunks.append(chunk)
+
+        return relevant_document_chunks
